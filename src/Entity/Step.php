cb2ca218aea5f29764df047274538ee209e10fc4
Mason Pike <mpike@flyamelia.com>
2023-11-20 14:18:51 +0100

    fix mistake with Step entity*

--- src/Entity/Step.php
+++ src/Entity/Step.php
@@ -17,6 +17,9 @@ class Step
     #[ORM\JoinColumn(nullable: false)]
     private ?Recipe $associatedRecipe = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $content = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -37,6 +40,18 @@ class Step
     public function setAssociatedRecipe(?Recipe $associatedRecipe): static
     {
         $this->associatedRecipe = $associatedRecipe;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
 
         return $this;
     }
